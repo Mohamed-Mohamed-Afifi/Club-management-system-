d1cbd58839c1f62676f138cbca5196f9f92a1587
Mohamed-Mohamed-Afifi <mohamed.org050@gmail.com>
2022-04-04 23:25:39 +0200

    add park to club

---
+++ src/club/Park.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package club;
+
+/**
+ *
+ * @author DELL 7510
+ */
+public class Park {
+    private String place;
+    private double num_of_members;
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public double getNum_of_members() {
+        return num_of_members;
+    }
+
+    public void setNum_of_members(double num_of_members) {
+        this.num_of_members = num_of_members;
+    }
+    
+}
